303a9cce8a19290fce261a22746f489abd877269
alisolarflare <alisunderstorm@gmail.com>
2017-08-23 15:49:49 -0400

    Removed Components: Metrics, Persistence, RTP

--- src/main/java/buttondevteam/presents/Main.java
+++ src/main/java/buttondevteam/presents/Main.java
@@ -7,10 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import buttondevteam.presents.dictionary.DictionaryComponent;
 import buttondevteam.presents.magic.MagicComponent;
-import buttondevteam.presents.metrics.MetricsComponent;
-import buttondevteam.presents.rtp.RandomTeleportComponent;
 import buttondevteam.presents.spawn.SpawnComponent;
-import buttondevteam.presents.test.TestComponent;
 
 public class Main extends JavaPlugin{
 	public void onEnable(){
@@ -20,10 +17,7 @@ public class Main extends JavaPlugin{
 		logger.info(pdfFile.getName() + " has been started (V." + pdfFile.getVersion()+ ").");
 		
 		new DictionaryComponent().register(this);
-		new RandomTeleportComponent().register(this);
-		new MetricsComponent().register(this);
 		new SpawnComponent().register(this);
-		new TestComponent().register(this);
 		new MagicComponent().register(this);
 		
 		logger.info(pdfFile.getName() + " has fully registered (V." + pdfFile.getVersion()+ ").");
