61aaec1ef66d5187bffa08dda111eb2925ac1105
alisolarflare <alisunderstorm@gmail.com>
2017-08-25 19:52:05 -0400

    Started work on Dungeon code

---
+++ src/main/java/buttondevteam/presents/dungeon/DungeonComponent.java
@@ -0,0 +1,16 @@
+package buttondevteam.presents.dungeon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import buttondevteam.presents.architecture.Component;
+
+public class DungeonComponent extends Component{
+
+	@Override
+	public void register(JavaPlugin plugin) {
+		this.registerCommand(plugin, new DungeonCreate());
+		this.registerCommand(plugin, new DungeonDelete());
+		
+	}
+	
+}
---
+++ src/main/java/buttondevteam/presents/dungeon/DungeonDelete.java
@@ -0,0 +1,39 @@
+package buttondevteam.presents.dungeon;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+import buttondevteam.lib.chat.CommandClass;
+import buttondevteam.presents.architecture.commands.ModCommand;
+
+@CommandClass(path="dungeon delete")
+public class DungeonDelete extends ModCommand{
+
+	@Override
+	public boolean OnCommand(Player player, String alias, String[] args) {
+		
+		if (args.length < 1) return false;
+		
+		String dungeonName = args[0];
+		FileConfiguration config = this.getPlugin().getConfig();
+		
+		if(!config.contains("dungeon." + dungeonName)){
+			player.sendMessage("This dungeon does not exist!");
+			String output = "Valid dungeons are:";
+			for(String key : config.getConfigurationSection("dungeon").getKeys(false)){
+				output += key;
+				output += ", ";
+			}
+			player.sendMessage(output);
+		}else{
+			for(String key : config.getConfigurationSection("dungeon." + dungeonName).getKeys(true)){
+				config.set("dungeon." + dungeonName + "." + key, null);
+			}
+			player.sendMessage("Dungeon " + dungeonName + " deleted.");
+		}
+		
+		
+		return true;
+	}
+
+}
