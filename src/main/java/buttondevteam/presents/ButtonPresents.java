7775203b5b6b3eca1453fd2f7fb858c5f9597890
NorbiPeti <szatmari.norbert.peter@gmail.com>
2018-08-05 17:49:58 +0200

    Integrated ChunkArchive & added test
    Testing on each start

--- src/main/java/buttondevteam/presents/ButtonPresents.java
+++ src/main/java/buttondevteam/presents/ButtonPresents.java
@@ -1,14 +1,14 @@
 package buttondevteam.presents;
 
-import java.util.logging.Logger;
-
-import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.java.JavaPlugin;
-
+import buttondevteam.presents.components.chunkarchive.ChunkArchiveComponent;
 import buttondevteam.presents.components.magic.MagicComponent;
 import buttondevteam.presents.components.research.ResearchComponent;
 import buttondevteam.presents.components.research.dictionary.DictionaryComponent;
 import buttondevteam.presents.components.spawn.SpawnComponent;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 public class ButtonPresents extends JavaPlugin{
 
@@ -23,6 +23,7 @@ public class ButtonPresents extends JavaPlugin{
 		new SpawnComponent().register(this);
 		new MagicComponent().register(this);
 		new ResearchComponent().register(this);
+        new ChunkArchiveComponent().register(this);
 		
 		logger.info(pdfFile.getName() + " has fully registered (V." + pdfFile.getVersion()+ ").");
 	}
